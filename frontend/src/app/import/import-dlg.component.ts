2b8d64380ac52ee0f060cc984823f0ec63b4301b
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2022-05-04 09:54:33 +0300

    add select checkboxes

--- frontend/src/app/import/import-dlg.component.ts
+++ frontend/src/app/import/import-dlg.component.ts
@@ -3,17 +3,21 @@ import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 import { Transaction } from '../models/transaction';
 
+interface TransactionSelection extends Transaction {
+  selected: boolean;
+}
+
 @Component({
   templateUrl: './import-dlg.component.html',
   styleUrls: ['./import-dlg.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImportDlgComponent {
-  readonly columns = ['date', 'amount', 'category', 'party', 'details'];
-  data: Transaction[] = [];
+  readonly columns = ['selected', 'date', 'amount', 'category', 'party', 'details'];
+  data: TransactionSelection[] = [];
 
   constructor(@Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext<Transaction[] | null, Transaction[]>) {
-    this.data = this.context.data;
+    this.data = this.context.data.map(t => ({...t, selected: !t.id}));
   }
 
   onNext() {
