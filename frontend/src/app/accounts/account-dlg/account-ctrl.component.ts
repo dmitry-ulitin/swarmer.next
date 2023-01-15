edddc440f41902522d678a58ebe74a99cfc19f53
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2023-01-15 10:45:23 +0200

    save progress

--- frontend/src/app/accounts/account-dlg/account-ctrl.component.ts
+++ frontend/src/app/accounts/account-dlg/account-ctrl.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
-import { ControlValueAccessor, UntypedFormArray, UntypedFormControl, UntypedFormGroup, NG_VALUE_ACCESSOR, Validators, NG_VALIDATORS, FormControl, ValidationErrors } from '@angular/forms';
+import { ControlValueAccessor, FormArray, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators, NG_VALIDATORS, ValidationErrors, UntypedFormArray } from '@angular/forms';
 import { Store } from '@ngxs/store';
 import { TuiDestroyService } from '@taiga-ui/cdk';
 import { takeUntil } from 'rxjs';
@@ -30,12 +30,12 @@ export class AccountCtrlComponent implements ControlValueAccessor {
   currencies = this.store.selectSnapshot(AccState.currencies);
   userCurrency = this.store.selectSnapshot(AppState.claims)?.currency || 'RUB';
 
-  form = new UntypedFormGroup({
-    'id': new UntypedFormControl(),
-    'fullname': new UntypedFormControl('', Validators.required),
-    'is_owner': new UntypedFormControl(true),
-    'is_coowner': new UntypedFormControl(false),
-    'is_shared': new UntypedFormControl(false),
+  form = new FormGroup({
+    'id': new FormControl(),
+    'fullname': new FormControl('', Validators.required),
+    'is_owner': new FormControl(true),
+    'is_coowner': new FormControl(false),
+    'is_shared': new FormControl(false),
     'accounts': new UntypedFormArray([]),
     'permissions': new UntypedFormArray([])
   });
@@ -44,8 +44,16 @@ export class AccountCtrlComponent implements ControlValueAccessor {
     return this.form.controls['accounts'] as UntypedFormArray;
   }
 
-  getAccount(index: number): UntypedFormGroup {
-    return this.accounts.controls[index] as UntypedFormGroup;
+  getAccount(index: number): FormGroup {
+    return this.accounts.controls[index] as FormGroup;
+  }
+
+  get permissions(): UntypedFormArray {
+    return this.form.controls['permissions'] as UntypedFormArray;
+  }
+
+  getPermission(index: number): FormGroup {
+    return this.permissions.controls[index] as FormGroup;
   }
 
   constructor(private store: Store, destroy$: TuiDestroyService) {
@@ -64,6 +72,7 @@ export class AccountCtrlComponent implements ControlValueAccessor {
       this.accounts.controls.forEach(c => c.get('start_balance')?.disable());
       this.accounts.controls.forEach(c => c.get('currency')?.disable());
     }
+    (value?.permissions || [null]).forEach(p => this.onAddPermission(p));
   }
 
   get canDelete(): boolean {
@@ -71,13 +80,13 @@ export class AccountCtrlComponent implements ControlValueAccessor {
   }
 
   onAddAccount(acc: any): void {
-    this.accounts.push(new UntypedFormGroup({
-      'id': new UntypedFormControl(acc?.id),
-      'name': new UntypedFormControl(acc?.name || ''),
-      'currency': new UntypedFormControl(acc?.currency || this.userCurrency),
-      'start_balance': new UntypedFormControl(acc?.start_balance),
-      'balance': new UntypedFormControl(acc?.balance),
-      'deleted': new UntypedFormControl(acc?.deleted)
+    this.accounts.push(new FormGroup({
+      'id': new FormControl(acc?.id),
+      'name': new FormControl(acc?.name || ''),
+      'currency': new FormControl(acc?.currency || this.userCurrency),
+      'start_balance': new FormControl(acc?.start_balance),
+      'balance': new FormControl(acc?.balance),
+      'deleted': new FormControl(acc?.deleted)
     }));
     this.accounts.controls.filter(a => !a.get('deleted')?.value)[0]?.get('name')?.enable();
   }
@@ -87,6 +96,34 @@ export class AccountCtrlComponent implements ControlValueAccessor {
     if (!this.canDelete) {
       this.accounts.controls.filter(a => !a.get('deleted')?.value)[0].get('name')?.disable();
     }
+  }
+
+  onAddPermission(p: any): void {
+    const group = new FormGroup({
+      'user': new FormGroup({
+        'id': new FormControl(p?.user?.id),
+        'email': new FormControl(p?.user?.email),
+        'name': new FormControl(p?.user?.name)
+      }),
+      'is_readonly': new FormControl(!!p?.is_readonly && !p?.is_admin),
+      'is_write': new FormControl(!p?.is_readonly || !!p?.is_admin),
+      'is_admin': new FormControl(p?.is_admin),
+    });
+    this.permissions.push(group);
+  }
+
+  onToggleAdmin(event: any, index: number): void {
+    if (event.target.checked) {
+      this.getPermission(index).get('is_readonly')?.setValue(false);
+      this.getPermission(index).get('is_write')?.setValue(true);
+      this.getPermission(index).get('is_write')?.disable();
+    } else {
+      this.getPermission(index).get('is_write')?.enable();
+    }
+  }
+
+  onRemovePermission(index: number): void {
+    this.permissions.removeAt(index);
   }
 
   validate({ value }: FormControl): ValidationErrors | null {
