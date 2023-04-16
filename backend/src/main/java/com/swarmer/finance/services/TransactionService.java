7fc8f25d013d9304bfa290ff780f79add3fee4d2
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2023-04-16 14:19:30 +0300

    fix summary

--- backend/src/main/java/com/swarmer/finance/services/TransactionService.java
+++ backend/src/main/java/com/swarmer/finance/services/TransactionService.java
@@ -250,6 +250,9 @@ public class TransactionService {
                         if (result.containsKey(rCurrency)) {
                             var rSummary = result.get(rCurrency);
                             rSummary.setTransfers_debit(rSummary.getTransfers_debit() + b.getCredit());
+                        } else {
+                            var rSummary = result.get(aCurrency);
+                            rSummary.setTransfers_debit(rSummary.getTransfers_debit() + b.getDebit());
                         }
                     }
                 } else {
@@ -265,6 +268,9 @@ public class TransactionService {
                         if (result.containsKey(aCurrency)) {
                             var aSummary = result.get(aCurrency);
                             aSummary.setTransfers_credit(aSummary.getTransfers_credit() + b.getDebit());
+                        } else {
+                            var aSummary = result.get(rCurrency);
+                            aSummary.setTransfers_credit(aSummary.getTransfers_credit() + b.getCredit());
                         }
                     }
                 } else {
